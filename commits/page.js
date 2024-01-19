6d9b53f5969ca593b836a816727e6eb0cf8ff3e4
César Silva <casrr@proton.me>
2024-01-19 17:12:17 -0600

    e

---
+++ commits/page.js
@@ -0,0 +1,16 @@
+import CommitList from "./CommitList"
+
+
+export default function Tickets() {
+  return (
+    <main>
+      <nav>
+        <h2>Commits</h2>
+        <p>Commits for repo</p>
+      </nav>
+      <CommitList />
+      
+    </main>
+    
+  )
+}
